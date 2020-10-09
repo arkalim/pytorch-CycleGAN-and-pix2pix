6616e81ac2fc4d7adf2fc73406db4f49872cf821
Abdur Rahman Kalim <arkhappiness@gmail.com>
2020-10-09 09:21:57 +0530

    tum_dataset modified

--- data/tum_dataset.py
+++ data/tum_dataset.py
@@ -1,63 +1,158 @@
 import os
-import SimpleITK as sitk
-from data.base_dataset import BaseDataset, get_transform, get_params
-from PIL import Image
+os.environ["TORCHIO_HIDE_CITATION_PROMPT"] = str(1)
+
+import os.path
+import random
+import torchvision.transforms as transforms
 import torch
+from data.base_dataset import BaseDataset
+# from data.image_folder import make_dataset
+import pickle
+import numpy as np
+import SimpleITK as sitk
+import torchio
+from torchio.transforms import (
+    RescaleIntensity,
+    RandomAffine,
+    RandomElasticDeformation,
+    Compose,
+    OneOf,
+    Crop,
+    Resample,
+    Pad,
+    RandomFlip
+)
+# import napari
+
+
+def load_image_file(path: str) -> np.ndarray:
+    img = sitk.ReadImage(path)
+    return img
 
 
 class TUMDataset(BaseDataset):
-    """A dataset class for MRI to US Dataset."""
+    """Add flag to visualize the 3D volume
+    """
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+        Returns:
+            the modified parser.
+        """
+        parser.add_argument('--visualize_volume', type=bool, default=False, help='Set visualize to False. it\'s only '
+                                                                                 'used for debugging.')
+        parser.add_argument('--load_mask', type=bool, default=False, help='load prostate mask for seg. loss')
+        return parser
 
     def __init__(self, opt):
+        """Initialize this dataset class.
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
         BaseDataset.__init__(self, opt)
+        self.opt = opt
+        self.root = opt.dataroot
+        self.load_mask = opt.load_mask
 
-        self.dataset_dir = opt.dataroot
-        self.transform = get_transform(opt)
+        self.patients = self.read_list_of_patients()
+        random.shuffle(self.patients)
+        self.subjects = {}
+        # self.mr = {}
+        # self.trus = {}
 
-        self.A_filenames = []
-        self.B_filenames = []
-        self.S_filenames = []
-        
-        for trial in os.listdir(self.dataset_dir):
-            trail_dir = os.path.join(self.dataset_dir, trial)
-            for patient in os.listdir(trail_dir):
-                patient_dir = os.path.join(trail_dir, patient, 'preprocessed/rigid/cropped')
-                A_path = os.path.join(patient_dir, 'mr.mhd')
-                B_path = os.path.join(patient_dir, 'trus.mhd')
-                S_path = os.path.join(patient_dir, 'mr_tree.mhd')
-
-            if os.path.exists(A_path) and os.path.exists(B_path) and os.path.exists(S_path):
-                self.A_filenames.append(os.path.join(patient_dir, 'mr.mhd'))
-                self.B_filenames.append(os.path.join(patient_dir, 'trus.mhd'))
-                self.S_filenames.append(os.path.join(patient_dir, 'mr_tree.mhd'))
-
-        if not (len(self.A_filenames) == len(self.B_filenames) == len(self.S_filenames)):
-            raise AssertionError('Dataset Length Mismatch')
-
+        assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
         self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
         self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
-                
+
+        self.input_size = [256] * 3
+        self.min_size = 64
+
+        self.transform = self.create_transforms()
+
+    def create_transforms(self):
+        # Let's use one preprocessing transform and one augmentation transform
+        # This transform will be applied only to scalar images:
+        rescale = RescaleIntensity((-1, 1))
+        transforms = [rescale]
+        # As RandomAffine is faster then RandomElasticDeformation, we choose to
+        # apply RandomAffine 80% of the times and RandomElasticDeformation the rest
+        # Also, there is a 25% chance that none of them will be applied
+        if self.opt.isTrain:
+            spatial = OneOf(
+                {RandomAffine(translation=5): 0.8, RandomElasticDeformation(): 0.2},
+                p=0.75,
+            )
+            transforms += [RandomFlip(axes=(0, 2), p=0.8), spatial]
+
+        self.ratio = self.min_size/np.max(self.input_size)
+        transforms.append(Resample(self.ratio))
+        crop_size = list(((np.array([85, 66, 79]) / self.ratio - self.input_size)/2).astype(np.int))
+        transforms.append(Crop(crop_size))
+        transform = Compose(transforms)
+        return transform
+
+    def reverse_resample(self, min_value=-1):
+        transforms = [Resample(1/self.ratio)]
+        pad_size = list(np.ceil((np.array([85, 66, 79]) - np.asarray(self.input_size) * self.ratio) / 2).astype(np.int))
+        return Compose(transforms + [Pad(pad_size, padding_mode=min_value)])
+
+    def read_list_of_patients(self):
+        patients = []
+        for root, dirs, files in os.walk(self.root):
+            if ('nonrigid' in root) or ('cropped' not in root):
+                continue
+            patients.append(root)
+        return patients
+
     def __getitem__(self, index):
-        
-        A = Image.fromarray(sitk.GetArrayFromImage(sitk.ReadImage(self.A_filenames[index]))[23])
-        B = Image.fromarray(sitk.GetArrayFromImage(sitk.ReadImage(self.B_filenames[index]))[23])
-        S = Image.fromarray(sitk.GetArrayFromImage(sitk.ReadImage(self.S_filenames[index]))[23])
+        sample, subject = self.load_subject_(index)
 
-        # apply the same transform to A, B and S
-        transform_params = get_params(self.opt, A.size)
-        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1), method=Image.NEAREST)
-        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1), method=Image.NEAREST)
-        S_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1), method=Image.NEAREST, segmentation=True)
+        transformed_ = self.transform(subject)
 
-        A = A_transform(A)
-        B = B_transform(B)
-        S = S_transform(S)
+        if self.opt.visualize_volume:
+            with napari.gui_qt():
+                napari.view_image(np.stack([transformed_['mr'].data.squeeze().numpy(),
+                                                     transformed_['trus'].data.squeeze().numpy()]))
 
-        ones = torch.ones_like(S, dtype=int)
-        minus_ones = torch.ones_like(S, dtype=int) * (-1)
-        S = torch.where(S > 0, ones, minus_ones)
+        dict_ = {
+            'Patient': sample.split('/')[-4],
+            'A_paths': sample + "/mr.mhd",
+            'B_paths': sample + "/trus.mhd"
+        }
 
-        return {'A': A, 'B': B, 'S':S}
+        true_A = transformed_['mr'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]]
+        true_B = transformed_['trus'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]]
+        mask = transformed_['mr_tree'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]]
+
+        dict_['A'] = true_A[:, 23, :, :]
+        dict_['B'] = true_B[:, 23, :, :]
+
+        ones = torch.ones_like(mask)
+        minus_ones = torch.ones_like(mask) * (-1)
+        dict_['S'] = torch.where(mask > 0, ones, minus_ones)[:, 23, :, :]
+        return dict_
+
+    def load_subject_(self, index):
+        # returns samples of dimension [channels, z, x, y]
+        sample = self.patients[index % len(self.patients)]
+        # data_folder = '/preprocessed/cropped/'
+        # load mr and turs file if it hasn't already been loaded
+        if sample not in self.subjects:
+            # print(f'loading patient {sample}')
+            
+            subject = torchio.Subject(mr=torchio.ScalarImage(sample + "/mr.mhd"),
+                                        trus=torchio.ScalarImage(sample + "/trus.mhd"),
+                                        mr_tree=torchio.LabelMap(sample + "/mr_tree.mhd"))
+
+            self.subjects[sample] = subject
+        subject = self.subjects[sample]
+        return sample, subject
 
     def __len__(self):
-        return len(self.A_filenames)  
+        return 5 * len(self.patients)
+
+    def name(self):
+        return 'TUMDataset'
